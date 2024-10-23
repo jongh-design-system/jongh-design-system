6d95bf9c64b7863c5779593a1a7db2b506b9e8d2
whdgur5717 <whdgur57117@gmail.com>
2024-10-24 01:25:24 +0900

    ui 빌드 위한 설정 변경

--- packages/ui/vite.config.ts
+++ packages/ui/vite.config.ts
@@ -1,27 +1,58 @@
 import { defineConfig } from "vite"
 import react from "@vitejs/plugin-react"
 import dts from "vite-plugin-dts"
-import { resolve } from "path"
+import path, { resolve } from "path"
 import tsconfigPaths from "vite-tsconfig-paths"
+import { fileURLToPath } from "url"
+
+//Fix: d.ts파일 생성시 경로를 절대경로가 아닌 상대경로로 참조하는 문제
+
+const __filename = fileURLToPath(import.meta.url)
+
+const __dirname = path.dirname(__filename)
 
 export default defineConfig({
-  plugins: [react(), dts(), tsconfigPaths()],
+  resolve: {
+    alias: [
+      {
+        find: "@styled-system",
+        replacement: path.join(__dirname, "./styled-system"),
+      },
+    ],
+  },
+  plugins: [react(), tsconfigPaths(), dts()],
   build: {
     lib: {
       entry: [resolve(__dirname, "src/components/index.ts")],
       fileName: (format) => `index.${format}.js`,
       name: "components",
+      formats: ["cjs", "es"],
     },
     rollupOptions: {
-      external: ["react", "react-dom"],
-      output: {
-        globals: {
-          react: "React",
-          "react-dom": "ReactDOM",
+      external: ["react", "react/jsx-runtime", "react-dom", "react-dom/client"],
+      treeshake: true,
+      output: [
+        {
+          format: "es",
+          entryFileNames: "[name].mjs",
+          globals: {
+            react: "React",
+            "react-dom": "ReactDOM",
+          },
         },
-      },
+        {
+          format: "cjs",
+          entryFileNames: "[name].cjs",
+          globals: {
+            react: "React",
+            "react-dom": "ReactDOM",
+          },
+        },
+      ],
     },
+
     sourcemap: true,
     emptyOutDir: true,
+    minify: false,
   },
 })
