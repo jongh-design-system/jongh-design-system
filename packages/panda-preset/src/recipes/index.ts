ef4a85e7cae64ae8544e17b4d5a8796745aa13f8
whdgur5717 <whdgur57117@gmail.com>
2024-11-03 15:55:44 +0900

    feat(recipes): 슬라이더 컴포넌트 레시피 추가

--- packages/panda-preset/src/recipes/index.ts
+++ packages/panda-preset/src/recipes/index.ts
@@ -5,12 +5,14 @@ import type {
 } from "@pandacss/dev"
 import { buttonRecipe } from "./button"
 import { accordionRecipe } from "./accordion"
+import { sliderRecipe } from "./slider"
 
 export const recipes: Record<
   string,
   Partial<RecipeConfig<RecipeVariantRecord>>
 > = {
   button: buttonRecipe,
+  slider: sliderRecipe,
 }
 
 export const slotRecipes: Record<string, Partial<SlotRecipeConfig>> = {
