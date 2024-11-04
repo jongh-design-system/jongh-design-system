d75c21615c372458eadd349df4166f33e804f927
whdgur5717 <whdgur57117@gmail.com>
2024-11-04 15:35:49 +0900

    feat(accordion): 수정된 스타일 및 애니메이션 속성 적용

--- packages/panda-preset/src/recipes/accordion.ts
+++ packages/panda-preset/src/recipes/accordion.ts
@@ -4,9 +4,7 @@ export const accordionRecipe = defineSlotRecipe({
   slots: ["root", "content", "item", "trigger"],
   base: {
     root: {
-      divideY: "1px",
-      borderTopWidth: "1px",
-      borderBottomWidth: "1px",
+      width: "100%",
     },
     trigger: {
       alignItems: "center",
@@ -21,21 +19,16 @@ export const accordionRecipe = defineSlotRecipe({
         cursor: "not-allowed",
       },
     },
+    item: {
+      borderBottom: "1px solid black",
+    },
     content: {
       overflow: "hidden",
-      transitionProperty: "padding-bottom",
-      transitionDuration: "normal",
-      transitionTimingFunction: "default",
-      _open: {
-        animation: "collapse-in",
-      },
-      _closed: {
-        animation: "collapse-out",
-      },
+
+      transitionTimingFunction: "cubic-bezier(0.4, 0, 0.2, 1)",
+      transitionDuration: "150ms",
+      height: "var(--accordion-height)",
     },
-  },
-  defaultVariants: {
-    size: "md",
   },
   variants: {
     size: {
