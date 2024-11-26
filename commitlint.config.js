9793aa94b7b3290621d9f13f3220686a506be340
whdgur5717 <whdgur57117@gmail.com>
2024-11-26 15:03:35 +0900

    chore(commitlint): add custom rules for commit message types

--- commitlint.config.js
+++ commitlint.config.js
@@ -1,3 +1,20 @@
 export default {
   extends: ["@commitlint/config-conventional"],
+  rules: {
+    "type-enum": [
+      2,
+      "always",
+      [
+        "feat",
+        "fix",
+        "docs",
+        "style",
+        "refactor",
+        "test",
+        "chore",
+        "revert",
+        "release",
+      ],
+    ],
+  },
 }
