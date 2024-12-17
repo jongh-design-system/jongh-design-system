4c2f28a3daef874c272f9356a874f2bc561c1f27
whdgur5717 <whdgur57117@gmail.com>
2024-12-17 14:44:53 +0900

    feat(cli): remove parseConfig from init function - not necessary at this point

--- packages/cli/src/init.ts
+++ packages/cli/src/init.ts
@@ -1,8 +1,6 @@
 import { checkPandaInit } from "./pre/checkPandaInit"
 import { initTemplate } from "./pre/initTemplate"
-// import { parseConfig } from "./pre/parseConfig.cjs"
 export async function init() {
   const root = await checkPandaInit()
   initTemplate(root)
-  // parseConfig()
 }
