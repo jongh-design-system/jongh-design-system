1bf330adf7f001b68d1f9b821cc12ea5c0527cf5
whdgur5717 <whdgur57117@gmail.com>
2024-12-21 16:43:27 +0900

    feat(cli): change tsup options - ems -> cjs

--- packages/cli/tsup.config.ts
+++ packages/cli/tsup.config.ts
@@ -1,14 +1,9 @@
 import { defineConfig } from "tsup"
-import fs from "fs-extra"
 
 export default defineConfig({
   entry: ["src/index.ts"],
-  format: ["esm"],
-  dts: true,
+  format: ["cjs"],
   clean: true,
   sourcemap: false,
   minify: true,
-  async onSuccess() {
-    await fs.copy("../ui/src/components", "./dist/components")
-  },
 })
