d2ebeb33bec710fa310409c9a1eccc6758a3a9e8
whdgur5717 <whdgur57117@gmail.com>
2025-01-24 15:45:52 +0900

    refactor(cli): add ConfigError handling for unresolved tsconfig imports in resolveImport function

--- packages/cli/src/add/utils/resolveImport.ts
+++ packages/cli/src/add/utils/resolveImport.ts
@@ -1,13 +1,19 @@
+import { ConfigError } from "../../common/error"
 import { createMatchPath, type ConfigLoaderSuccessResult } from "tsconfig-paths"
 
 export async function resolveImport(
   importPath: string,
   config: Pick<ConfigLoaderSuccessResult, "absoluteBaseUrl" | "paths">,
 ) {
-  return createMatchPath(config.absoluteBaseUrl, config.paths)(
+  const match = createMatchPath(config.absoluteBaseUrl, config.paths)(
     importPath,
     undefined,
     () => true,
-    [".ts", ".tsx"],
   )
+  if (!match) {
+    throw new ConfigError("cannot resolve your tsconfig import", {
+      cause: "no match found",
+    })
+  }
+  return match
 }
