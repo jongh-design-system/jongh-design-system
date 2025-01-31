2e63644bfb9c04f5f55079bc29c4fe636d49cc5a
whdgur5717 <whdgur57117@gmail.com>
2025-01-31 23:31:33 +0900

    test(cli): add tests for radix-import transformation - test for namespace import

---
+++ packages/cli/src/test/radix-import.test.ts
@@ -0,0 +1,31 @@
+import { beforeAll, describe, expect, test } from "vitest"
+import fs from "fs-extra"
+import path from "path"
+import { transform } from "../radix-import/transform"
+import { Project } from "ts-morph"
+
+describe("radix-import", () => {
+  beforeAll(async () => {})
+
+  test("namespace import test", () => {
+    const input = fs.readFileSync(
+      path.join(__dirname, "./fixture/namespace_input.tsx"),
+      "utf-8",
+    )
+    const output = fs.readFileSync(
+      path.join(__dirname, "./fixture/namespace_output.tsx"),
+      "utf-8",
+    )
+
+    const project = new Project()
+    const sourceFile = project.createSourceFile("test.tsx", input, {
+      overwrite: true,
+    })
+
+    transform(sourceFile)
+
+    expect(sourceFile.getFullText().replace(/\s+/g, "")).toBe(
+      output.replace(/\s+/g, ""),
+    )
+  })
+})
