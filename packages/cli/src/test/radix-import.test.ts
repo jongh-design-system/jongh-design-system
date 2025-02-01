bfe9a67d84763474678208e321b1e9a33c5cffa4
whdgur5717 <whdgur57117@gmail.com>
2025-02-02 00:00:50 +0900

    test(cli): add test for identical component name

--- packages/cli/src/test/radix-import.test.ts
+++ packages/cli/src/test/radix-import.test.ts
@@ -50,4 +50,26 @@ describe("radix-import", () => {
       output.replace(/\s+/g, ""),
     )
   })
+
+  test("현재 사용하고 있던 컴포넌트명과 변경되어야 할 컴포넌트명이 동일할 경우", () => {
+    const input = fs.readFileSync(
+      path.join(__dirname, "./fixture/named_input_2.tsx"),
+      "utf-8",
+    )
+    const output = fs.readFileSync(
+      path.join(__dirname, "./fixture/named_output_2.tsx"),
+      "utf-8",
+    )
+
+    const project = new Project()
+    const sourceFile = project.createSourceFile("test.named_2.tsx", input, {
+      overwrite: true,
+    })
+
+    transform(sourceFile)
+
+    expect(sourceFile.getFullText().replace(/\s+/g, "")).toBe(
+      output.replace(/\s+/g, ""),
+    )
+  })
 })
