0ce3f9178d8c1272d6277c40f0d6afe42560372f
whdgur5717 <whdgur57117@gmail.com>
2025-02-04 21:23:14 +0900

    feat(ui): enhance Button component className merge

--- packages/ui/src/component/button/ui/index.tsx
+++ packages/ui/src/component/button/ui/index.tsx
@@ -2,25 +2,25 @@ import { recipe, ButtonVariantProps } from "./recipe"
 import type { ComponentPropsWithoutRef } from "react"
 import { forwardRef } from "react"
 import { Slot } from "radix-ui"
-import { cx } from "@styled-system/css"
+import { css, cx } from "@styled-system/css"
 
 export type ButtonProps = ComponentPropsWithoutRef<"button"> & {
   asChild?: boolean
 } & ButtonVariantProps
 
 export const Button = forwardRef<HTMLButtonElement, ButtonProps>(
-  ({ asChild, ...props }, ref) => {
+  ({ asChild, className, ...props }, ref) => {
     const Comp = asChild ? Slot.Root : "button"
     const [variantProps, componentProps] = recipe.splitVariantProps(props)
-    const styles = recipe(variantProps)
+    const styles = recipe.raw(variantProps)
 
     return (
       <Comp
         role="button"
         ref={ref}
-        className={cx(styles, componentProps?.className)}
+        className={cx(css(styles), className)}
         {...componentProps}
-      ></Comp>
+      />
     )
   },
 )
